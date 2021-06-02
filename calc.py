0e35e9c0b2fcaefda90da7f074de0ce8ace26e95
artiomorlovalex <artiomorlovalex@gmail.com>
2021-06-02 17:36:14 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,19 +2,9 @@
 print("Привет! Это калькулятор!")
 number1 = input(float("Введи левый операнд "))
 number2 = input(float("Введи правый операнд"))
-operator = input("Ведите операцию (+, -, *, /, **): ")
+operator = input("Ведите операцию (+, -, *, /, **): ") 
 if (operator == "+"):
    print("Сумма значений = ", (number1 + number2)
 elif (operator == "-"):
    print("Разность значений = ",  (number1 - number2)
 elif (operator == "*"):
-   print("Произведение значений = ",  (number1 * number2)
-elif (operator == "/"):
-   if (number2 == 0):
-      print("На ноль делить нельзя!")
-   else:
-      print("Частное значений = ", (number1 / number2)
-elif (operator == "**"):
-      print("Степень значений = ", (number1 ** number2)
-else:
-    print("Вы неправильно ввели операцию или ничего не ввели! Попробуйте ещё раз.") 
