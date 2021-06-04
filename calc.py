28cc0e5c324f4506f323673435109bfb0432156f
artiomorlovalex <artiomorlovalex@gmail.com>
2021-06-04 09:52:33 +0300

    Fixed syntax errors

--- calc.py
+++ calc.py
@@ -1,20 +1,21 @@
-#Программааааааа а Питончике версияя 1.0
+#Программа калькулятор на Python версия 6.0
 print("Привет! Это калькулятор!")
-number1 = input(float("Введи левый операнд "))
-number2 = input(float("Введи правый операнд"))
+number1 = float(input("Введи левый операнд "))
+number2 = float(input("Введи правый операнд"))
 operator = input("Ведите операцию (+, -, *, /, **): ") 
-if (operator == "+"):
-   print("Сумма значений = ", (number1 + number2)
-elif (operator == "-"):
-   print("Разность значений = ",  (number1 - number2)
-elif (operator == "*"):
-   print("Произведение значений = ",  (number1 * number2)
-elif (operator == "/"):
-   if (number2 == 0):
+if operator == "+" or operator == "-" or operator == "*" or operator == "/" or operator == "**":
+  if (operator == "+"):
+    print("Сумма значений = ", (number1 + number2))
+  elif (operator == "-"):
+    print("Разность значений = ",  (number1 - number2))
+  elif (operator == "*"):
+    print("Произведение значений = ",  (number1 * number2))
+  elif (operator == "**"):
+    print("Степень значений = ", (number1 ** number2))
+  elif (operator == "/"):
+    if (number2 == 0):
       print("На ноль делить нельзя!")
-   else:
-       print("Частное значений = ", (number1 / number2)
-elif (operator == "**"):
-      print("Степень значений = ", (number1 ** number2)
+    else:
+      print("Частное значений = ", (number1 / number2))
 else:
-    print("Вы неправильно ввели операцию или ничего не ввели! Попробуйте ещё раз.") 
+  print("Вы неправильно ввели операцию или ничего не ввели! Попробуйте ещё раз.") 
